ab0aae9e47023b39be2943dbfab4c8ceb546b595
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-10-24 08:23:22 +0500

    added exercise task 1 from https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/2_Arrays/2_arrays_exercise.md

---
+++ Arrays_exercise.py
@@ -0,0 +1,40 @@
+expenses = [
+    2200,
+    2350,
+    2600,
+    2130,
+    2190
+]
+
+# 1. In Feb, how many dollars you spent extra compare to January?
+
+extraDollar = expenses[1]- expenses[0]
+print(extraDollar)
+
+# 2. Find out your total expense in first quarter (first three months) of the year.
+total = 0
+for x in range(0,3):
+    total +=expenses[x]
+print(total)
+
+# 3. Find out if you spent exactly 2000 dollars in any month
+# My version
+for i in expenses:
+    if i == 2000:
+        print("Yes")
+
+# better version
+print("Did i spent 2000$ in any month ?",2000 in expenses)
+
+# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+
+expenses.append(1980)
+
+
+# 5. You returned an item that you bought in a month of April and
+# got a refund of 200$. Make a correction to your monthly expense list
+# based on this
+
+expenses[3] = expenses[3] - 200
+print(expenses)
+
