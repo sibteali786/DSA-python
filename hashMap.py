b1df134c3573a9830d5ecdb47d3be0402fc3f837
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-11-06 16:34:32 +0500

    Added hash table topic

---
+++ hashMap.py
@@ -0,0 +1,7 @@
+stock_prices = []
+with open("stock_prices.csv","r") as f:
+    for line in f:
+        tokens = line.split(',')
+        day = tokens[0]
+        price = float(tokens[1])
+        stock_prices.append([day,price])
